b1d90088809cc84e6ffbcdb28f2f5acf1621e9c0
Lunnatica <paulalopezantelo@gmail.com>
2024-07-07 22:10:21 +0100

    fix: tests

--- jest.config.js
+++ jest.config.js
@@ -16,4 +16,10 @@ module.exports = {
       },
     ],
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!react-dnd|dnd-core|react-dnd-html5-backend)/",
+  ],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "<rootDir>/src/test/styleMock.js",
+  },
 };
